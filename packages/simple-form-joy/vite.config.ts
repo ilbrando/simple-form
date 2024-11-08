5b883c780babdbff3e39c5038bc5873615f0487e
Michael Brandt <michael@ilbrando.dk>
2024-11-08 15:10:39 +0100

    simple-form-joy: migrate from rollup to vite

---
+++ packages/simple-form-joy/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      formats: ["es", "cjs"],
+      name: "simple-form-joy",
+      fileName: "index"
+    },
+    outDir: "lib"
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "src/": `${path.resolve(__dirname, "src")}/`
+    }
+  }
+});
