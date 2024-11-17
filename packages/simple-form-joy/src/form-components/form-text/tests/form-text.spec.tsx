a517bef5d0f2c4bbca6ecfd3d4966db167d736bf
Michael Brandt <michael@ilbrando.dk>
2024-11-17 11:02:50 +0100

    Validate form state value

--- packages/simple-form-joy/src/form-components/form-text/tests/form-text.spec.tsx
+++ packages/simple-form-joy/src/form-components/form-text/tests/form-text.spec.tsx
@@ -3,14 +3,22 @@ import { expect, test } from "@playwright/experimental-ct-react";
 import { TestComponent } from "./test-component";
 
 test("updates form state when receiving input", async ({ mount }) => {
+  // Arrange
   let nameValue: string | null = null;
+  const expected = "abc";
 
   // Act
-  const component = await mount(<TestComponent />);
+  const component = await mount(
+    <TestComponent
+      valueChange={v => {
+        nameValue = v;
+      }}
+    />
+  );
 
   const textBox = component.getByRole("textbox");
-  await textBox.fill("abc");
+  await textBox.fill(expected);
 
   // Assert
-  expect(nameValue).toBe(null);
+  expect(nameValue).toBe(expected);
 });
--- packages/simple-form-joy/src/form-components/form-text/tests/test-component.tsx
+++ packages/simple-form-joy/src/form-components/form-text/tests/test-component.tsx
@@ -2,8 +2,13 @@ import { FormText } from "../form-text";
 import { useTestForm } from "../../../test-components/form-utils";
 import { TestWrapper } from "../../../test-components/test-wrapper";
 
-export const TestComponent = () => {
+type TestComponentProps = {
+  valueChange: ReturnType<typeof useTestForm>["fm"]["onChange"]["name"];
+};
+export const TestComponent = (props: TestComponentProps) => {
+  const { valueChange } = props;
   const { fm } = useTestForm(false);
+  fm.onChange.name = valueChange;
 
   return (
     <TestWrapper>
