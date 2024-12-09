8a7d11c2290f61c00d28b7108dd0ef3338c37620
ilbrando <michael@ilbrando.dk>
2024-12-09 14:31:04 +0100

    Tests for array and common validation rules

--- packages/simple-form/src/form-validation/validation-rules/array.test.ts
+++ packages/simple-form/src/form-validation/validation-rules/array.test.ts
@@ -2,57 +2,55 @@ import { describe, test } from "vitest";
 import { renderHook } from "@testing-library/react";
 
 import { useArrayValidationRules } from "./array";
-import { assertValidationResult } from "./validation-test-utils";
-
-const genericErrorMessage = "##ERROR##";
+import { assertValidationResult, genericErrorMessage } from "./validation-test-utils";
 
 describe("array", () => {
   test.each`
-    data      | count | expected
-    ${[1, 2]} | ${1}  | ${genericErrorMessage}
-    ${[1, 2]} | ${3}  | ${genericErrorMessage}
-    ${[1, 2]} | ${2}  | ${undefined}
-    ${[]}     | ${0}  | ${undefined}
-  `("count($data, $count) => $expected", ({ data, count, expected }) => {
+    value     | parameter | expected
+    ${[1, 2]} | ${1}      | ${genericErrorMessage}
+    ${[1, 2]} | ${3}      | ${genericErrorMessage}
+    ${[1, 2]} | ${2}      | ${undefined}
+    ${[]}     | ${0}      | ${undefined}
+  `("count($parameter)($value) => $expected", ({ value, parameter, expected }) => {
     // Arrange
     const { result } = renderHook(() => useArrayValidationRules());
 
     // Act
-    const actual = result.current.count(count)(data);
+    const actual = result.current.count(parameter)(value);
 
     // Assert
     assertValidationResult(expected, actual);
   });
 
   test.each`
-    data      | count | expected
-    ${[1, 2]} | ${1}  | ${genericErrorMessage}
-    ${[1, 2]} | ${3}  | ${undefined}
-    ${[1, 2]} | ${2}  | ${undefined}
-    ${[]}     | ${0}  | ${undefined}
-  `("maxCount($data, $count) => $expected", ({ data, count, expected }) => {
+    value     | parameter | expected
+    ${[1, 2]} | ${1}      | ${genericErrorMessage}
+    ${[1, 2]} | ${3}      | ${undefined}
+    ${[1, 2]} | ${2}      | ${undefined}
+    ${[]}     | ${0}      | ${undefined}
+  `("maxCount($parameter)($value) => $expected", ({ value, parameter, expected }) => {
     // Arrange
     const { result } = renderHook(() => useArrayValidationRules());
 
     // Act
-    const actual = result.current.maxCount(count)(data);
+    const actual = result.current.maxCount(parameter)(value);
 
     // Assert
     assertValidationResult(expected, actual);
   });
 
   test.each`
-    data      | count | expected
-    ${[1, 2]} | ${1}  | ${undefined}
-    ${[1, 2]} | ${3}  | ${genericErrorMessage}
-    ${[1, 2]} | ${2}  | ${undefined}
-    ${[]}     | ${0}  | ${undefined}
-  `("minCount($data, $count) => $expected", ({ data, count, expected }) => {
+    value     | parameter | expected
+    ${[1, 2]} | ${1}      | ${undefined}
+    ${[1, 2]} | ${3}      | ${genericErrorMessage}
+    ${[1, 2]} | ${2}      | ${undefined}
+    ${[]}     | ${0}      | ${undefined}
+  `("minCount($parameter)($value) => $expected", ({ value, parameter, expected }) => {
     // Arrange
     const { result } = renderHook(() => useArrayValidationRules());
 
     // Act
-    const actual = result.current.minCount(count)(data);
+    const actual = result.current.minCount(parameter)(value);
 
     // Assert
     assertValidationResult(expected, actual);
