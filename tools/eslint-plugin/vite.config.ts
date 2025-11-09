d6f1513505c4660a0678ee329ca5af9b036be0d9
ilbrando <michael@ilbrando.dk>
2025-11-09 21:33:14 +0100

    Include typescript-eslint plugin

--- tools/eslint-plugin/vite.config.ts
+++ tools/eslint-plugin/vite.config.ts
@@ -2,13 +2,9 @@ import { defineConfig } from "vite";
 import path from "path";
 import { builtinModules } from "module";
 
-//const builtinModules = ["crypto", "events", "fs", "fs/promises", "module", "os", "path", "stream", "util", "url"];
-
 const NODE_BUILT_IN_MODULES = builtinModules.filter(m => !m.startsWith("_"));
 NODE_BUILT_IN_MODULES.push(...NODE_BUILT_IN_MODULES.map(m => `node:${m}`));
-const noReload = process.env.VITE_NO_RELOAD !== undefined;
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
@@ -18,12 +14,10 @@ export default defineConfig({
     },
     outDir: "lib",
     sourcemap: true,
-    target: "es2022", // choose ES target appropriate for your node support
+    target: "es2022",
     rollupOptions: {
-      // do not bundle peer deps or TS ESLint internals
       external: ["eslint", "@typescript-eslint/utils", "@typescript-eslint/parser", "@typescript-eslint/eslint-plugin", "eslint-config-prettier", "@eslint/js", ...builtinModules, ...builtinModules.map(m => `node:${m}`)],
       output: {
-        // ensure named exports preserved
         exports: "named"
       }
     }
